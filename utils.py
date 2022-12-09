275489d9b4cefeb6a6327df098f9d5aa960ac91c
Rafael-Bardisa <72401330+Rafael-Bardisa@users.noreply.github.com>
2022-12-09 20:20:31 +0100

    geojson a medias

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from unidecode import unidecode
+
+
 def acronym(x: str):
     """
     given x the name$acronym of a political party, return the acronym
@@ -8,7 +11,6 @@ def acronym(x: str):
     
 def proper(x: str):
     """
-    
     strips city name x from all leading and trailing whitespace and properly capitalizes first word
     if city has two accepted names, uses first accepted form
     """
@@ -30,3 +32,21 @@ def compute_popular(df):
     
     return df
     
+
+def rtve_norm(x: str):
+    """
+    removes all capitalization and accents from x
+    """
+    x_list = unidecode(x).lower().replace("'", "").replace("(", "").replace(")", "").strip().split(" ")
+    return "-".join(reorder_muni_name(x_list))
+
+
+def reorder_muni_name(x):
+    """
+    reorders x, a list, to be compliant with rtve normalized names
+    """
+    prefixes = ["a", "o", "as", "es", "l", "la", "el", "las", "los", "els", "les"]
+    
+    if x[0] in prefixes:
+        return x[1:] + [x[0]]
+    return x
